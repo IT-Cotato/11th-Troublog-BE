2b5fa1cd91873cb9571bafaa75a4f2bd8fba111a
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-06-01 13:49:46 +0900

    🎉 Initial Commit

---
+++ src/main/java/troublog/backend/TroublogApplication.java
@@ -0,0 +1,13 @@
+package troublog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroublogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroublogApplication.class, args);
+	}
+
+}
