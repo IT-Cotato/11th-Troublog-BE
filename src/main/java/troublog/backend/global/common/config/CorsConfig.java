13406ab2190926e8fe30e0ede5f00abccdfa5208
jay91537 <veuxft@gmail.com>
2025-07-06 17:34:59 +0900

    [Feat] Configuration추가

---
+++ src/main/java/troublog/backend/global/common/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package troublog.backend.global.common.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	private static final String CLIENT_LOCAL = "http://localhost:3000";
+	private static final String SERVER_LOCAL = "http://localhost:8080";
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin(CLIENT_LOCAL);
+		config.addAllowedOrigin(SERVER_LOCAL);
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.setExposedHeaders(List.of(
+			"accessToken",
+			"Authorization",
+			"refreshToken"
+		));
+		config.addExposedHeader("Set-Cookie");
+
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
---
+++ src/main/java/troublog/backend/global/common/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package troublog.backend.global.common.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+import troublog.backend.global.common.filter.ExceptionHandlerFilter;
+import troublog.backend.global.common.filter.JwtAuthenticationFilter;
+import troublog.backend.global.common.util.JwtAuthenticationProvider;
+import troublog.backend.global.common.util.JwtProvider;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtAuthenticationProvider jwtAuthenticationProvider;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final ExceptionHandlerFilter exceptionHandlerFilter;
+	private final CorsFilter corsFilter;
+
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilter(corsFilter);
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(EndpointRequest.toAnyEndpoint())
+				.permitAll()
+				.requestMatchers("/auth/**", "/swagger-ui/**", "v3/api-docs/**", "/error", "/")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+			);
+		http
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class);
+
+		return http.build();
+	}
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		builder.authenticationProvider(jwtAuthenticationProvider);
+		return builder.build();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
