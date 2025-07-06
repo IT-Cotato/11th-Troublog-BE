cc49d79d8ef37139681652da809109e6b22e1324
jay91537 <veuxft@gmail.com>
2025-07-06 17:35:56 +0900

    [Feat] 프론트, 서버 환경변수 EnvType 추가

---
+++ src/main/java/troublog/backend/global/common/constant/EnvType.java
@@ -0,0 +1,35 @@
+package troublog.backend.global.common.constant;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EnvType {
+
+	LOCAL("local"),
+	DEV("dev"),
+	PROD("prod"),
+	// 테스트는 임시로
+	TEST("test");
+
+	private final String envType;
+
+	public static EnvType valueOfEnvType(String clientEnvType) {
+		return Arrays.stream(values())
+			.filter(et -> et.envType.equals(clientEnvType))
+			.findFirst()
+			.orElse(null);
+	}
+
+	public static boolean isEqualEnvType(EnvType serverEnvType, EnvType clientEnvType) {
+		return Objects.equals(serverEnvType, clientEnvType);
+	}
+
+	public boolean isTestOrLocal() {
+		return this == TEST || this == LOCAL;
+	}
+}
