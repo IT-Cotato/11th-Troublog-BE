14e27b92b4b48330809d75d6de2a7041620168d3
신윤섭 <zksdbstjq@gmail.com>
2025-07-17 16:13:36 +0900

    ✨ feat: ErrorTag 클래스에 Lombok 어노테이션 추가 (#18)

--- src/main/java/troublog/backend/domain/trouble/entity/ErrorTag.java
+++ src/main/java/troublog/backend/domain/trouble/entity/ErrorTag.java
@@ -11,19 +11,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import troublog.backend.global.common.entity.BaseEntity;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
 @Table(name = "error_tag")
 public class ErrorTag extends BaseEntity {
 
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id", nullable = false)
 	Post Post;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "error_tag_id")
 	private Long id;
+
 	@NotNull
 	@Column(name = "name")
 	private String name;
