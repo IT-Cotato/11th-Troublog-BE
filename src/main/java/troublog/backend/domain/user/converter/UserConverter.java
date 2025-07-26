3960b31989e05c70bbd2fc58571554be4034fdbd
jay91537 <veuxft@gmail.com>
2025-07-26 17:27:00 +0900

    [Feat] Follow, User Converter추가

---
+++ src/main/java/troublog/backend/domain/user/converter/UserConverter.java
@@ -0,0 +1,35 @@
+package troublog.backend.domain.user.converter;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import lombok.experimental.UtilityClass;
+import troublog.backend.domain.user.dto.UserDto;
+import troublog.backend.domain.user.entity.User;
+
+@UtilityClass
+public class UserConverter {
+
+	public Set<Long> extractUserIds(List<User> users) {
+		return users.stream()
+			.map(User::getId)
+			.collect(Collectors.toSet());
+	}
+
+	public List<UserDto.UserFollowsDto> toUserFollowsDtoList(List<User> users, Set<Long> viewerFollowingIds) {
+		return users.stream()
+			.map(user -> toUserFollowsDto(user, viewerFollowingIds))
+			.toList();
+	}
+
+	public UserDto.UserFollowsDto toUserFollowsDto(User user, Set<Long> viewerFollowingIds) {
+		return new UserDto.UserFollowsDto(
+			user.getId(),
+			user.getNickname(),
+			user.getEmail(),
+			user.getProfileUrl(),
+			viewerFollowingIds.contains(user.getId())
+		);
+	}
+}
