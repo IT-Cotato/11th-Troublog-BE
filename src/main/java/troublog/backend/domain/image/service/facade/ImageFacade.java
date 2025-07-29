8b8267dd558dcc95b2dc65c5679957a42d2fc8b5
신윤섭 <zksdbstjq@gmail.com>
2025-07-29 11:36:10 +0900

    ✨ feat: 썸네일, 프로필 이미지 관리를 위한 ImageFacade 클래스 추가 (#21)

--- src/main/java/troublog/backend/domain/image/service/facade/ImageFacade.java
+++ src/main/java/troublog/backend/domain/image/service/facade/ImageFacade.java
@@ -1,75 +1,27 @@
 package troublog.backend.domain.image.service.facade;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import troublog.backend.domain.image.converter.PostImageConverter;
-import troublog.backend.domain.image.entity.PostImage;
-import troublog.backend.domain.image.service.command.PostImageCommandService;
-import troublog.backend.domain.image.service.query.PostImageQueryService;
 import troublog.backend.domain.image.service.s3.S3Uploader;
-import troublog.backend.domain.trouble.entity.Post;
 
+/**
+ * 썸네일 이미지, 유저 프로필 이미지 저장 및 삭제을 위한 클래스
+ */
 @Slf4j
-@Service
-@Transactional
+@Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageFacade {
-	public static final String POST_DIR = "post/";
-
 	private final S3Uploader s3Uploader;
-	private final PostImageCommandService postImageCommandService;
-	private final PostImageQueryService postImageQueryService;
 
-	public PostImage savePostImage(Post post, MultipartFile file) {
-		return uploadAndSaveImage(file, false, post.getId());
+	public String saveImage(MultipartFile file, String dirName) {
+		return s3Uploader.uploadSingleImage(file, dirName).join();
 	}
 
-	public PostImage savePostImageWithThumbnail(Post post, MultipartFile file) {
-		return uploadAndSaveImage(file, true, post.getId());
-	}
-
-	public List<PostImage> savePostImages(Long postId, List<MultipartFile> files) {
-		return s3Uploader.uploadMultipleImages(files, POST_DIR + postId)
-			.thenApply(imageUrls -> {
-				List<PostImage> postImages = PostImageConverter.toEntityList(imageUrls);
-				return postImageCommandService.saveAll(postImages);
-			})
-			.join();
-	}
-
-	public void deletePostImage(Long postImageId) {
-		PostImage postImage = postImageQueryService.findById(postImageId);
-
-		s3Uploader.deleteImage(postImage.getImageUrl())
-			.thenRun(() -> postImageCommandService.delete(postImage))
-			.join();
-	}
-
-	public void deletePostImages(Long postId) {
-		List<PostImage> postImages = postImageQueryService.findAllByPostId(postId);
-
-		if (postImages.isEmpty()) {
-			return;
-		}
-
-		s3Uploader.deleteMultipleImages(PostImageConverter.toImageUrlList(postImages))
-			.thenRun(() -> postImageCommandService.deleteAll(postImages))
-			.join();
-	}
-
-	private PostImage uploadAndSaveImage(MultipartFile file, boolean isThumbnail, Long postId) {
-		return s3Uploader.uploadSingleImage(file, POST_DIR + postId)
-			.thenApply(imageUrl -> {
-				PostImage postImage = PostImageConverter.toEntity(imageUrl, isThumbnail);
-				return postImageCommandService.save(postImage);
-			})
-			.join();
+	public void deleteImage(String imageUrl) {
+		s3Uploader.deleteImage(imageUrl);
 	}
 }
