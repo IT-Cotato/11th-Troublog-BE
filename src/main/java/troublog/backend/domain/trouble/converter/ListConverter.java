7a58fb4243db4dfdeb4765aa85bcdcf4b2f8d69b
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-08-15 20:18:29 +0900

    ✨ feat: 트러블슈팅 요약본 목록 응답에 summaryPostId 필드 추가 (#69)

--- src/main/java/troublog/backend/domain/trouble/converter/ListConverter.java
+++ src/main/java/troublog/backend/domain/trouble/converter/ListConverter.java
@@ -3,6 +3,7 @@ package troublog.backend.domain.trouble.converter;
 import lombok.experimental.UtilityClass;
 import troublog.backend.domain.trouble.dto.response.TroubleListResDto;
 import troublog.backend.domain.trouble.entity.Post;
+import troublog.backend.domain.trouble.entity.PostSummary;
 import troublog.backend.domain.trouble.service.facade.query.PostQueryFacade;
 
 @UtilityClass
@@ -22,6 +23,22 @@ public class ListConverter {
 			.isVisible(post.getIsVisible())
 			// .summaryType(PostValidator.isValidSummaryContent(post) ?
 			// 	SummaryType.getName(post.getContents().getFirst().getSummaryType()) : null)
+			.build();
+	}
+
+	public static TroubleListResDto toAllSummerizedListResDto(PostSummary postSummary) {
+		return TroubleListResDto.builder()
+			.id(postSummary.getPost().getId())
+			.summaryPostId(postSummary.getId())
+			.projectId(postSummary.getPost().getProject().getId())
+			.title(postSummary.getPost().getTitle())
+			.date(postSummary.getCreated_at() != null ? postSummary.getCreated_at() : null)
+			.status(String.valueOf(postSummary.getPost().getStatus()))
+			.starRating(postSummary.getPost().getStarRating() != null ? postSummary.getPost().getStarRating().getValue() : null)
+			.imageUrl(postSummary.getPost().getThumbnailUrl())
+			.error(PostQueryFacade.findErrorTag(postSummary.getPost()))
+			.techs(PostQueryFacade.findTopTechStackTags(postSummary.getPost()))
+			.isVisible(postSummary.getPost().getIsVisible())
 			.build();
 	}
 }
