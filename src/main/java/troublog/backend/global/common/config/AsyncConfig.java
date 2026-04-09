67734e3b081184e9b7b3592e65e3dea74af06075
신윤섭 <zksdbstjq@gmail.com>
2026-04-09 13:49:36 +0900

    ♻️ refactor: AsyncConfig 비동기 처리 개선
    Ref: #TRB-23
    Signed-off-by: 신윤섭 <zksdbstjq@gmail.com>

--- src/main/java/troublog/backend/global/common/config/AsyncConfig.java
+++ src/main/java/troublog/backend/global/common/config/AsyncConfig.java
@@ -2,16 +2,20 @@ package troublog.backend.global.common.config;
 
 import java.util.Map;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import org.slf4j.MDC;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -19,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
-	private static final String SUMMARY_EXECUTOR_PREFIX = "AI-Summary";
+	private static final String SUMMARY_EXECUTOR_PREFIX = "Ai";
 	private static final String IMAGE_EXECUTOR_PREFIX = "Image";
 	private static final String MAIL_EXECUTOR_PREFIX = "Mail";
 
@@ -40,24 +44,34 @@ public class AsyncConfig implements AsyncConfigurer {
 
 	@Override
 	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-		return (ex, method, params) ->
+		return (ex, method, params) -> {
+			Sentry.captureException(ex);
 			log.error("[Async] 비동기 작업 중 예외 발생 - Method: {}, Params: {}", method.getName(), params, ex);
+		};
 	}
 
 	private Executor createVirtualThreadExecutor(String threadNamePrefix) {
-		SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
-		executor.setThreadNamePrefix(threadNamePrefix + "-");
-		executor.setVirtualThreads(true);
-		executor.setTaskDecorator(new MdcPropagatingTaskDecorator());
+		ThreadFactory virtualThreadFactory = createVirtualThreadFactory(threadNamePrefix);
+		TaskExecutorAdapter executor = new TaskExecutorAdapter(
+			Executors.newThreadPerTaskExecutor(virtualThreadFactory)
+		);
+		executor.setTaskDecorator(new MdcAndSentryTaskDecorator());
 
 		log.info("[Async] {} Virtual Thread Executor 초기화 완료", threadNamePrefix);
 		return executor;
 	}
 
-	private static class MdcPropagatingTaskDecorator implements TaskDecorator {
+	private ThreadFactory createVirtualThreadFactory(String threadNamePrefix) {
+		return Thread.ofVirtual()
+			.name(threadNamePrefix + "-", 0)
+			.factory();
+	}
+
+	private static class MdcAndSentryTaskDecorator implements TaskDecorator {
 
 		@Override
-		public Runnable decorate(Runnable runnable) {
+		@NonNull
+		public Runnable decorate(@NonNull Runnable runnable) {
 			Map<String, String> contextMap = MDC.getCopyOfContextMap();
 
 			return () -> {
@@ -66,10 +80,18 @@ public class AsyncConfig implements AsyncConfigurer {
 						MDC.setContextMap(contextMap);
 					}
 					runnable.run();
+				} catch (Exception ex) {
+					Sentry.captureException(ex);
+					sneakyThrow(ex);
 				} finally {
 					MDC.clear();
 				}
 			};
+		}
+
+		@SuppressWarnings("unchecked")
+		private static <T extends Throwable> void sneakyThrow(Throwable t) throws T {
+			throw (T) t;
 		}
 	}
 }
